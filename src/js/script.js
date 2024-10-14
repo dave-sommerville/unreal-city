4a5a0fa22d9e4f4f31cf5edc22853141c7cff5f3
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-10-13 20:59:44 -0500

    Change in files, removal of first banner

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict'
+
+const elementOne = document.querySelector('.one');
+const elementTwo = document.querySelector('.two');
+const elementThree = document.querySelector('.three');
+const elementFour = document.querySelector('.four');
+const elementFive = document.querySelector('.five');
+const elementSix = document.querySelector('.six');
+const elementHorizon = document.querySelector('.horizon');
+
+function applyEffectWithDelay() {
+    setTimeout(() => {
+        elementOne.classList.add('active');
+        elementTwo.classList.add('active');
+        elementThree.classList.add('active');
+        elementFour.classList.add('active');
+        elementFive.classList.add('active');
+        elementSix.classList.add('active');
+        elementHorizon.classList.add('active');
+    }, 1000); 
+}
+window.onload = applyEffectWithDelay;
+
